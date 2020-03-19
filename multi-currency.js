a08d86a10beda12c50751b991ccf98b051e01210
bamacaerick <bamacaerick@gmail.com>
2020-03-18 20:13:39 -0600

    [Update] column iteration to add new TD element

--- multi-currency.js
+++ multi-currency.js
@@ -2,16 +2,29 @@ $(document).ready(function() {
   $("#btnAddCol").click(function() {
     var totalRows = countRows();
     var bodyRows = $("[id^='ibtr_']");
-    var headRow = $("#tINBOUND > thead > tr");
+    var headRow = $("#tINBOUND > thead > tr > th").eq(4); // NOTE: There are 2 hidden columns (th) elements
+    var headRowNewContent = $("<th><div style='width:100px!important; height:auto;'>Sell Rate EUR</div></th>"); // Create new head element to append
+    var $footerRow = $('#tINBOUND > tfoot > tr > td:first-child'); // Getting the first TD element from footer
 
-    // Insert column -> "Sell Rate" ( en posicion 5 -> en base a los valores de la columna 4 ) NOTA: hay 1 columna escondida [0]
-    var cellHead = headRow[0].insertCell(5);
-    cellHead.innerHTML = "Sell Rate EUR";
-    for (index = 0; index < bodyRows.length; index++) {
-      var cell = bodyRows[index].insertCell(5);
-      var newValue = bodyRows.find("td:eq(4)").html(); // debe tomar el valor de la celda al lado izq.!
-      cell.innerHTML = "EUR " + convertValueToEUR(newValue).toFixed(5);
-    }
+    headRow.after(headRowNewContent); // Adding a new <TH> element next to its left sibling
+
+    $.each(bodyRows, function(index, value){
+      var $currentTdObject = $(value).find('td').eq(4);
+      var currentAmout = $currentTdObject.text();
+      var $eurValueToAdd = $("<td/>", {
+        text: "EUR " + convertValueToEUR(currentAmout).toFixed(5),
+      });
+
+      $currentTdObject.after($eurValueToAdd);
+    });
+
+    $footerRow.attr('colspan', '4'); // Updating footer span to include new EUR column
+    
+    // for (index = 0; index < bodyRows.length; index++) {
+    //   var cell = bodyRows[index].insertCell(5);
+    //   var newValue = bodyRows.find("td:eq(4)").html(); // debe tomar el valor de la celda al lado izq.!
+    //   cell.innerHTML = "EUR " + convertValueToEUR(newValue).toFixed(5);
+    // }
 
     // Insert column -> "Revenue"
     // var cellHead = headRow[0].insertCell(7);
